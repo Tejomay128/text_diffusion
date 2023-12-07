42dd1a886fc9eeb963443d82f7c0a7de2c1a107e
Tejomay128 <tejomaypadole128@gmail.com>
2023-12-07 18:16:55 +0530

    writing the training loop

--- train.py
+++ train.py
@@ -9,10 +9,14 @@ from basic_utils import (
     load_defaults_config,
     add_dict_to_argparser,
     load_model_emb,
-    load_tokenizer
+    load_tokenizer,
+    create_model_and_diffusion,
+    args_to_dict
 )
 
 from datasets_utils import load_data_text
+
+device = torch.device("cuda:0")
 
 def create_argparser():
     defaults = dict()
@@ -27,4 +31,29 @@ def main():
 
     tokenizer = load_tokenizer(args)
     embs, tokenizer = load_model_emb(args, tokenizer)
+
+    data = load_data_text(
+        batch_size=args.batch_size,
+        seq_len=args.seq_len,
+        data_args = args,
+        loaded_vocab=tokenizer,
+        model_emb = embs # use model's weights as init
+    )
+
+    data_valid = load_data_text(
+        batch_size=args.batch_size,
+        seq_len=args.seq_len,
+        data_args=args,
+        split='valid',
+        deterministic=True,
+        loaded_vocab=tokenizer,
+        model_emb=embs # using the same embedding wight with tranining data
+    )
+
+    model, diffusion = create_model_and_diffusion(**args_to_dict(args, load_defaults_config().keys()))
+    model.to(device)
+
+    timstep_sampler = create_named_schedule_sampler(args.schedule_sampler, diffusion)
+
+    
 
